c4c577f7c2549840b4c53f948c280c02830064ff
Nico_surface <nico.kobler@gmx.ch>
2018-04-17 16:15:56 +0200

    Merge remote-tracking branch 'origin/develop' into develop
    # Conflicts:
    #	src/main/java/rcas/model/MagicFormulaTireModel.java
    #	src/main/java/rcas/model/TireModel.java
    #	src/rcas/model/MagicFormulaTireModel.java
    #	src/rcas/model/TireModel.java
    #	src/rcas/model/tire/MagicFormulaTireModel.java
    #	src/rcas/model/tire/TireModel.java

--- src/main/java/rcas/controller/RCASMainModalController.java
+++ src/main/java/rcas/controller/RCASMainModalController.java
@@ -1,6 +1,12 @@
 package rcas.controller;
 
+import com.sun.javafx.binding.BidirectionalBinding;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.StringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -11,31 +17,62 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import rcas.model.RaceCar;
 
+
 public class RCASMainModalController {
 
     @FXML
     private ListView<RaceCar> carView;
 
     @FXML
-    private TextField name;
+    private TextField addName;
 
-    public static final ObservableList<RaceCar> data =
+    @FXML
+    private TextField detailName;
+
+    @FXML
+    private TextField leftFrontTier;
+
+    @FXML
+    private TextField rightFrontTier;
+
+    @FXML
+    private TextField leftBackTire;
+
+    @FXML
+    private TextField rightBackTier;
+
+    @FXML
+    private TextField fronttrack;
+
+    @FXML
+    private TextField reartrack;
+
+    @FXML
+    private TextField wheelbase;
+
+    @FXML
+    private TextField cogheight;
+
+    @FXML
+    private TextField frontrolldist;
+
+    private ObservableList<RaceCar> data =
             FXCollections.observableArrayList();
+
+    private RaceCar selectedCar;
 
     @FXML
     public void initialize() {
         carView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-        carView.setOnMouseClicked(new EventHandler<MouseEvent>() {
 
+        carView.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent click) {
-
                 if (click.getClickCount() == 2) {
                     //Use ListView's getSelected Item
-                    RaceCar car = carView.getSelectionModel()
+                    selectedCar = carView.getSelectionModel()
                             .getSelectedItem();
-                    System.out.println(car.getName());
-                    //use this to do whatever you want to. Open Link etc.
+                    carBindings();
                 }
             }
         });
@@ -44,11 +81,24 @@ public class RCASMainModalController {
     @FXML
     private void add(ActionEvent event) {
         RaceCar raceCar = new RaceCar(0, 0, 0, 0);
-        raceCar.setName(name.getText());
+        raceCar.setName(addName.getText());
         data.add(raceCar);
         carView.setItems(data);
-        name.clear();
+        addName.clear();
     }
 
+    public void carBindings() {
+        detailName.textProperty().bind(selectedCar.nameProperty());
+        leftFrontTier.textProperty().bind(selectedCar.cornerWeightFLProperty().asString());
+        rightFrontTier.textProperty().bind(selectedCar.cornerWeightFRProperty().asString());
+        leftBackTire.textProperty().bind(selectedCar.cornerWeightRLProperty().asString());
+        rightBackTier.textProperty().bind(selectedCar.cornerWeightRRProperty().asString());
+
+        fronttrack.textProperty().bind(selectedCar.frontTrackProperty().asString());
+        reartrack.textProperty().bind(selectedCar.rearTrackProperty().asString());
+        wheelbase.textProperty().bind(selectedCar.wheelbaseProperty().asString());
+        cogheight.textProperty().bind(selectedCar.cogHeightProperty().asString());
+        frontrolldist.textProperty().bind(selectedCar.frontRollDistProperty().asString());
+    }
 
 }
